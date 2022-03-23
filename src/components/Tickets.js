35ef029c48f3a0092540aa140955949ead667e6c
Estevam <estevamfurtado@gmail.com>
2022-03-23 20:56:48 -0300

    Escondendo preenchimento de dados em funcao os seats selecionados

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,37 +1,69 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
 import SeatsMap from "./SeatsMap";
 
 
-export default function Tickets () {
-    return (
+export default function Tickets() {
+
+    const { sessionId } = useParams();
+
+    const [session, setSession] = useState(null);
+    const [selectedSeats, setSelectedSeats] = useState([]);
+    const [costumers, setCostumers] = useState([]);
+
+    const link = `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionId}/seats`;
+    useEffect(() => {
+        const promessa = axios.get(link);
+        promessa.then(resposta => {
+            console.log(resposta.data);
+            setSession(resposta.data);
+        }).catch(error => {
+            console.log("error in ", error)
+        })
+    }, []);
+
+    // escrever logica depois
+    function validateCostumers () {
+        return (costumers.length > 0);
+    }
+
+
+    const loading = (<p>Loading...</p>);
+    const loadedContent = (session ?
         <>
             <h1>Selecione os assentos</h1>
+
             <SeatsMap />
 
-            <h2>Informe os dados dos clientes</h2>
-            <div className="clientInputs">
-                <div className="clientInput">
-                    <label>Nome</label>
-                    <input></input>
-                </div>
-                <div className="clientInput">
-                    <label>CPF</label>
-                    <input></input>
-                </div>
-            </div>
+            {selectedSeats.length > 0 ?
+                (<>
+                    <h2>Informe os dados dos clientes</h2>
+                    {selectedSeats.map(() => {
+                        return (
+                            <div className="clientInputs">
+                                <div className="clientInput">
+                                    <label>Nome</label>
+                                    <input></input>
+                                </div>
+                                <div className="clientInput">
+                                    <label>CPF</label>
+                                    <input></input>
+                                </div>
+                            </div>)
+                    })}
+                </>) : <></>
+            }
 
-            <div className="clientInputs">
-                <div className="clientInput">
-                    <label>Nome</label>
-                    <input></input>
-                </div>
-                <div className="clientInput">
-                    <label>CPF</label>
-                    <input></input>
-                </div>
-            </div>
+            {validateCostumers()
+            ? (
+            <Link to="/sucesso">
+                <button>Reservar assento</button>
+            </Link>)
+            : <></>}
 
-            <Link to="/sucesso"><button>Reservar assento</button></Link>
-        </>
+        </> : <></>
     )
+
+    return (session ? loadedContent : loading);
 }
