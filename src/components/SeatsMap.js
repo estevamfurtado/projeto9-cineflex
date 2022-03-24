a5957e796701243d375b095b658f5ea1b523f3ed
Estevam <estevamfurtado@gmail.com>
2022-03-23 22:30:25 -0300

    Mapa funcionando

--- src/components/SeatsMap.js
+++ src/components/SeatsMap.js
@@ -1,3 +1,42 @@
-export default function SeatsMap () {
-    return (<div>SeatsMap</div>)
+import { useEffect, useState } from "react";
+
+
+export default function SeatsMap ({seats, selectSeat}) {
+
+    const seatsElements = (
+        <div className="seatsmap__elements">
+            {seats.map(seat => {
+                const {id, name, status} = seat;
+                let color = "gray";
+                switch (status) {
+                    case 'selected':
+                        color = "green";
+                        break;
+                    case 'unavailable':
+                        color = "yellow";
+                        break;
+                    default:
+                        break;
+                }
+                return <div key={id} id={id} className={`seatsmap__seat ${color}`} onClick={() => {
+                    selectSeat(id);
+                }}>{name}</div>
+            })}
+        </div>
+    )
+
+    const legenda = (
+        <div className="seatsmap__legenda">
+            <div><div className="seatsmap__seat green"></div><p>Selecionado</p></div>
+            <div><div className="seatsmap__seat gray"></div><p>Disponível</p></div>
+            <div><div className="seatsmap__seat yellow"></div><p>Indisponível</p></div>
+        </div>
+    );
+
+    return (
+    <div className="seatsmap">
+        {seatsElements}
+        {legenda}
+    </div>
+    )
 }
--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -9,8 +9,32 @@ export default function Tickets() {
     const { sessionId } = useParams();
 
     const [session, setSession] = useState(null);
-    const [selectedSeats, setSelectedSeats] = useState([]);
-    const [costumers, setCostumers] = useState([]);
+    const [seats, setSeatsData] = useState([]);
+    const [costumersData, setCostumersData] = useState([]);
+
+    const selectedSeats = seats.filter(seat => seat.status === 'selected');
+
+    function selectSeat (seatId) {
+        const newSeats = [...seats];
+        const newCostumersData = [...costumersData];
+
+        newSeats.forEach((seat, id) => {
+            if (seat.id === seatId) {
+                switch(seat.status){
+                    case "available":
+                        seat.status = "selected";
+                        newCostumersData.push({name: null, cpf: null})
+                        break;
+                    case "selected":
+                        seat.status = "available";
+                        newCostumersData.splice(id, 1);
+                        break;
+                }
+            }
+        })
+        setSeatsData(newSeats);
+        setCostumersData(newCostumersData);
+    }
 
     const link = `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionId}/seats`;
     useEffect(() => {
@@ -18,6 +42,14 @@ export default function Tickets() {
         promessa.then(resposta => {
             console.log(resposta.data);
             setSession(resposta.data);
+            const processedSeats = resposta.data.seats.map(seat => {
+                const {id, name} = seat;
+                let status = "unavailable";
+                if (seat.isAvailable) {status = "available";}
+                return {id: id, name: name, status: status};
+            });
+            //console.log('processed seats', processedSeats);
+            setSeatsData(processedSeats);
         }).catch(error => {
             console.log("error in ", error)
         })
@@ -25,7 +57,7 @@ export default function Tickets() {
 
     // escrever logica depois
     function validateCostumers () {
-        return (costumers.length > 0);
+        return (costumersData.length > 0);
     }
 
 
@@ -34,21 +66,21 @@ export default function Tickets() {
         <>
             <h1>Selecione os assentos</h1>
 
-            <SeatsMap />
+            <SeatsMap seats={seats} selectSeat={selectSeat}/>
 
-            {selectedSeats.length > 0 ?
+            {costumersData.length > 0 ?
                 (<>
                     <h2>Informe os dados dos clientes</h2>
-                    {selectedSeats.map(() => {
+                    {costumersData.map((costumer, idx) => {
                         return (
-                            <div className="clientInputs">
+                            <div className="clientInputs" key={idx}>
                                 <div className="clientInput">
                                     <label>Nome</label>
-                                    <input></input>
+                                    <input placeholder="Digite o nome do ticket..."></input>
                                 </div>
                                 <div className="clientInput">
                                     <label>CPF</label>
-                                    <input></input>
+                                    <input type="number" placeholder="Digite os números do cpf do ticket..."></input>
                                 </div>
                             </div>)
                     })}
